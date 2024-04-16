0cadbfee6435a62f95b1ab596279e0eee3613a71
rhqkfdl <nsh1044@naver.com>
2024-04-16 12:28:57 +0900

    commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = "dic_random",
-    version = "0.0.1",
+    version = "0.0.2",
     packages = ["dic_random"],
     classifiers = [
         "Programming Language :: Python :: 3",
